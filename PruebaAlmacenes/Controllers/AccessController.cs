879a2c72f4f357d7b722271c1b363cc4f99bdda3
Cesar Gutierrez <cesargutierrezgt@gmail.com>
2021-12-01 22:44:22 -0600

    Creacion de AccessController - View Index

---
+++ PruebaAlmacenes/Controllers/AccessController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaAlmacenes.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+    }
+}
