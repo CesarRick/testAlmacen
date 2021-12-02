39b5af65f3ed6a99e0524f8426f82b50edcf3c1a
Cesar Gutierrez <cesargutierrezgt@gmail.com>
2021-12-01 23:31:42 -0600

    Creacion de login

--- PruebaAlmacenes/Controllers/AccessController.cs
+++ PruebaAlmacenes/Controllers/AccessController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+// Modelos
+using PruebaAlmacenes.Models;
 
 namespace PruebaAlmacenes.Controllers
 {
@@ -14,6 +17,36 @@ namespace PruebaAlmacenes.Controllers
             return View();
         }
 
-        // 
+        // Login de usuario
+        public ActionResult Login(string userName, string password)
+        {
+            try
+            {
+                // Conexion
+                using (IncomelDBEntities db = new IncomelDBEntities())
+                {
+                    var lstUsuario = from d in db.Usuario
+                                     where d.NombreUsuario == userName && d.Contrasena == password && d.EstadoID == 1
+                                     select d;
+
+                    if (lstUsuario.Count() > 0)
+                    {
+                        Usuario usuario = lstUsuario.First();
+                        Session["User"] = usuario;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario no existe o no es correcto.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                return Content("Ocurrió un error: " + ex.Message);
+            }
+        }
+
     }
 }
